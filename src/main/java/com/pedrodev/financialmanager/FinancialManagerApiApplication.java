fdb110d24ee5775a42ab9476b9de9421a02af559
Pedro-Reis11 <pedroverissimoreis@gmail.com>
2026-03-11 14:25:15 -0300

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/pedrodev/financialmanager/FinancialManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedrodev.financialmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagerApiApplication.class, args);
+	}
+
+}
